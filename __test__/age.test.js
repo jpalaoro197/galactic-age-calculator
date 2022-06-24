7865086f351eee62e7de532ed21deacf7837482c
jacobpalaoro <jpalaoro197@gmail.com>
2022-06-24 15:03:10 -0700

    rewrite test to resemble new age constructor.

--- __test__/age.test.js
+++ __test__/age.test.js
@@ -3,21 +3,15 @@ import Age from './../src/age.js';
 describe('age', ()=> {
 
   test('should return age for age for each age property', () => {
-    const age = new Age(25, 4, 5, 3, 7);
+    const age = new Age(25, 27);
     expect(age.userAge).toEqual(25);
-    expect(age.mercuryAge).toEqual(4);
-    expect(age.venusAge).toEqual(5);
-    expect(age.marsAge).toEqual(3);
-    expect(age.jupiterAge).toEqual(7);
+    expect(age.averageLifeExpectancy).toEqual(27);
   })
 
   test('should make mercury age = .24 of user age', () => {
-  let mercuryAge = new Age(25, 0., 0, 0, 0);
-  expect(age.userAge).toEqual(25);
-  expect(age.mercuryAge.mercuryEquation()).toEqual(6);
-  expect(age.venusAge).toEqual(5);
-  expect(age.marsAge).toEqual(3);
-  expect(age.jupiterAge).toEqual(7);
+  let mercuryAgeTest = new Age(25, 0);
+ 
   });
 });
+
 
