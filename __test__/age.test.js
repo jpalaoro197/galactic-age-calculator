ea91018ec2e2d429560b6101e6624bdc541ea8e3
jacobpalaoro <jpalaoro197@gmail.com>
2022-06-24 13:27:10 -0700

    change to.equal to toEqual.

--- __test__/age.test.js
+++ __test__/age.test.js
@@ -1,13 +1,13 @@
-
+import Age from './../src/age.js';
 
 describe('age', ()=> {
 
-  test('should return age for age for each age property', ()=> {
-    const age = new age(25, 4, 5, 3, 7);
-    expect(age.userAge).to.equal(25);
-    expect(age.mercuryAge).to.equal(4);
-    expect(age.venusAge).to.equal(5);
-    expect(age.marsAge).to.equal(3);
-    expect(age.jupiterAge).to.equal(7);
+  test('should return age for age for each age property', () => {
+    const age = new Age(25, 4, 5, 3, 7);
+    expect(age.userAge).toEqual(25);
+    expect(age.mercuryAge).toEqual(4);
+    expect(age.venusAge).toEqual(5);
+    expect(age.marsAge).toEqual(3);
+    expect(age.jupiterAge).toEqual(7);
   })
 })
