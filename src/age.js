70f084177ee88a28de0a421c67409c00d789e9fc
jacobpalaoro <jpalaoro197@gmail.com>
2022-06-24 13:44:50 -0700

    add export default class age, with constructor.

---
+++ src/age.js
@@ -0,0 +1,10 @@
+export default class Age {
+  constructor(userAge, mercuryAge, venusAge, marsAge, jupiterAge ) {
+  this.userAge = userAge;
+  this.mercuryAge = mercuryAge;
+  this.venusAge = venusAge;
+  this.marsAge = marsAge;
+  this.jupiterAge = jupiterAge;
+  };
+  
+};
