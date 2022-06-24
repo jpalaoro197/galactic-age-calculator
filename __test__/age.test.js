b31b7eb893d2b4fd90ab1c1bbc4cc252e7490885
jacobpalaoro <jpalaoro197@gmail.com>
2022-06-24 14:26:59 -0700

    write a test for prototype mercuryEquation

--- __test__/age.test.js
+++ __test__/age.test.js
@@ -10,4 +10,14 @@ describe('age', ()=> {
     expect(age.marsAge).toEqual(3);
     expect(age.jupiterAge).toEqual(7);
   })
-})
+
+  test('should make mercury age = .24 of user age', () => {
+  let mercuryAge = new Age(25, 0., 0, 0, 0);
+  expect(age.userAge).toEqual(25);
+  expect(age.mercuryAge.mercuryEquation()).toEqual(6);
+  expect(age.venusAge).toEqual(5);
+  expect(age.marsAge).toEqual(3);
+  expect(age.jupiterAge).toEqual(7);
+  });
+});
+
